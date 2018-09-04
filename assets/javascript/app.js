7a8180b623d834c7b4cb7cd48c31ced351e07bc8
theinfamousAK <akhilkalepu@protonmail.com>
2018-09-03 20:14:06 -0400

    initial site files, basic layout and questions done

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+// ------------------------------------------------
+// ------------------------VARIABLES------------------------
+var right = 0;
+var wrong = 0;
+var unanswered = 0;
+
+var trivia = [
+    q: "When was the War of 1812?"
+    , a1: "1776", a2: "1812", a3: "1813", a4: "1912";
+    q: "How many hydrogen atoms are in a water molecule?"
+    , a1: "One", a2: "Two", a3: "Three", a4: "None";
+    q: "Mixing blue and yellow makes what color?"
+    , a1: "Red", a2: "Light Blue", a3: "Orange", a4: "Green";
+    q: "Who wrote str.italics(War and Peace)?"
+    , a1: "Noam Chomsky", a2: "Mark Twain", a3: "Leo Tolstoy", a4: "J.K. Rowling";
+    q: "What is the capital of Burkina Faso?"
+    , a1: "Ouagadougou", a2: "Monrovia", a3: "Lagos", a4: "Kinshasa";
+    q: "What is the definition of obnubilate?"
+    , a1: "To inhale through the nose.", a2: "To weave in and out.", a3: "To liquify by applying heat.", a4: "To darken or obscure something.";
+    q: "How long is a lightyear?"
+    , a1: "642 billion hours", a2: "5.9 trillion miles", a3: "3.14 quadrillion miles", a4: "6,537 years";
+    q: "Who invented the internet?"
+    , a1: "Al Gore", a2: "Jeff Bezos", a3: "Tupac Shakur", a4: "H.P. Lovecraft";
+];
+
+// ------------------------------------------------
+});
