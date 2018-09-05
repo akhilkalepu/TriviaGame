5c39207fe11398b41734802d1b5dfd7bb95721ed
theinfamousAK <akhilkalepu@protonmail.com>
2018-09-05 02:32:01 -0400

    answer function working, still resets after answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,10 @@
+$(document).ready(function () {
 // ------------------------VARIABLES------------------------
+var userChoice = "";
+
+var seconds = 30;
+var intervalId;
+
 var right = 0;
 var wrong = 0;
 var unanswered = 0;
@@ -6,76 +12,185 @@ var unanswered = 0;
 var trivia = [{
         question: "When was the War of 1812?",
         choices: ["1776", "1812", "1813", "1912"],
-        answer: "choice2"
+        answer: 1
     },
     {
         question: "How many hydrogen atoms are in a water molecule?",
         choices: ["One", "Two", "Three", "None"],
-        answer: "choice2"
+        answer: 1
     },
     {
         question: "Mixing blue and yellow makes what color?",
         choices: ["Red", "Light Blue", "Orange", "Green"],
-        answer: "choice4"
+        answer: 3
     },
     {
         question: "Who wrote str.italics(War and Peace)?",
         choices: ["Noam Chomsky", "Mark Twain", "Leo Tolstoy", "J.K. Rowling"],
-        answer: "choice3"
+        answer: 2
     },
     {
         question: "What is the capital of Burkina Faso?",
         choices: ["Ouagadougou", "Monrovia", "Lagos", "Kinshasa"],
-        answer: "choice1"
+        answer: 0
     },
     {
         question: "What is the definition of obnubilate?",
         choices: ["To inhale through the nose.", "To weave in and out.", "To liquify by applying heat.", "To darken or obscure something."],
-        answer: "choice4"
+        answer: 3
     },
     {
         question: "How long is a lightyear?",
         choices: ["642 billion hours", "5.9 trillion miles", "3.14 quadrillion miles", "6,537 years"],
-        answer: "choice2"
+        answer: 1
     },
     {
         question: "Who invented the internet?",
         choices: ["Al Gore", "Jeff Bezos", "Tupac Shakur", "H.P. Lovecraft"],
-        answer: "choice3"
+        answer: 2
     }
 ];
 
-// ------------------------------------------------
-
-$(document).ready(function () {
-
-// ------------------------START GAME------------------------
+// ------------------------GAME FUNCTIONS------------------------
 $("#quiz-box").hide();
-$("#start-btn").click(function () {
+
+i = 0;
+
+$("#start-btn").on("click", function () {
+    $("#start-box").html("");
     $("#quiz-box").slideToggle();
-    $(this).hide();
 });
-// ------------------------QUESTIONS------------------------
-var i = 0;
-for (i = 0; i < trivia.length; i++) {
-    $(".question-box").text(trivia[i].question);
-    $("#choice1").text(trivia[i].choices[0]);
-    $("#choice2").text(trivia[i].choices[1]);
-    $("#choice3").text(trivia[i].choices[2]);
-    $("#choice4").text(trivia[i].choices[3]);
-};
+$("#start-btn").on("click", quizfunction);
 
-// ------------------------ANSWERS------------------------
-// var choice1 = document.getElementById("choice1");
-// var choice2 = document.getElementById("choice2");
-// var choice3 = document.getElementById("choice3");
-// var choice4 = document.getElementById("choice4");
+function quizfunction() {
+    ask();
+    time();
+}
+    function ask () {
+        $(".question-box").text(trivia[i].question);
+        $("#choice1").text(trivia[i].choices[0]);
+        $("#choice2").text(trivia[i].choices[1]);
+        $("#choice3").text(trivia[i].choices[2]);
+        $("#choice4").text(trivia[i].choices[3]);
+    }
+    function time () {
+        var seconds = 30;
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+        $(".time-remaining").text(seconds);
+    }
+    function decrement() {
+        seconds--;
+        $(".time-remaining").text(seconds);
+        if (seconds === 0) {
+            stop();
+            alert("Time Up!");
+        }
+    }
+    function stop () {
+        clearInterval(intervalId);
+        var userChoice = "";
+    }
 
-// var userChoice  = document.getElementById(onclick);
+// ------------------------CHOICE BUTTONS------------------------
+$("#choice1").on("click", function () {
+    var userChoice = 0;
+    if (userChoice === trivia[i].answer) {
+        alert(userChoice);
+        alert(trivia[i].answer);
+        alert("Correct!");
+    } else {
+        alert(userChoice);
+        alert(trivia[i].answer);
+        alert("Wrong!");
+    }
+    i++;
+    quizfunction();
+});
+$("#choice2").on("click", function () {
+    var userChoice = 1;
+    if (userChoice === trivia[i].answer) {
+        alert(userChoice);
+        alert(trivia[i].answer);
+        alert("Correct!");
+    } else {
+        alert(userChoice);
+        alert(trivia[i].answer);
+        alert("Wrong!");
+    }
+    i++;
+    quizfunction();
+});
+$("#choice3").on("click", function () {
+    var userChoice = 2;
+    if (userChoice === trivia[i].answer) {
+        alert(userChoice);
+        alert(trivia[i].answer);
+        alert("Correct!");
+    } else {
+        alert(userChoice);
+        alert(trivia[i].answer);
+        alert("Wrong!");
+    }
+    i++;
+    quizfunction();
+});
+$("#choice4").on("click", function () {
+    var userChoice = 3;
+    if (userChoice === trivia[i].answer) {
+        alert(userChoice);
+        alert(trivia[i].answer);
+        alert("Correct!");
+    } else {
+        alert(userChoice);
+        alert(trivia[i].answer);
+        alert("Wrong!");
+    }
+    i++;
+    quizfunction();
+});
 
-// if (userChoice === trivia[i].answer) {
-//     alert("You are correct!");
-// };
+// ------------------------RIGHT/WRONG BUTTONS------------------------
+// function rightwrong () {
+//     if (userChoice === trivia[i].answer) {
+//         alert(userChoice);
+//         alert(trivia[i].answer);
+//         alert("Correct!");
+//     }   else {
+//         alert(userChoice);
+//         alert(trivia[i].answer);
+//         alert("Wrong!");
+//     }
+//     i++;
+//     quizfunction();
+// }
+
+// ------------------------POPUP FUNCTION------------------------
+// var roundseconds = 4;
+// var roundId;
+
+// function roundfunction() {
+//     popup();
+//     roundtime();
+// }
+//     function popup() {
+//         $("#form").hide();
+//         $("#quiz-box").html("<div>You are jkhjiuweaakbrjhdhkjh!</div>");
+//     }
+//     function roundtime() {
+//         clearInterval(roundId);
+//         roundId = setInterval(decrement, 1000);
+//     }
+//     function decrement() {
+//         roundseconds--;
+//         if (roundseconds === 0) {
+//             stop();
+//             alert("ASDFASDF");
+//         }
+//     }
+//     function stop() {
+//         clearInterval(roundId);
+//     }
+
 // ------------------------------------------------
-
 });
